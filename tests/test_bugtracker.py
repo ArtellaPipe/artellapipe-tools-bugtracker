d2b5137c62a7150130dc21bb725a6ec69e267666
Tomi Poveda <tpovedatd@gmail.com>
2019-10-28 21:01:59 +0100

    Updated code to fit PEP8 standards and added version test

--- tests/test_bugtracker.py
+++ tests/test_bugtracker.py
@@ -1,5 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains tests for artellapipe-tools-bugtracker
+"""
+
 import pytest
 
+from artellapipe.launcher import __version__
 
-def test_hello():
-    return True
+
+def test_version():
+    assert __version__.__version__
